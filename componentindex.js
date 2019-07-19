66ca23868a5f4c37bc88e87b561464503a010ca3
Manik Hosen <rahathossenmanik@gmail.com>
2019-07-20 01:52:49 +0600

    React JS Introductory & Basic Codes

---
+++ componentindex.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Car from './App';
+import * as serviceWorker from './serviceWorker';
+ReactDOM.render(<Car />, document.getElementById('root'));
---
+++ withjsx.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const myelement = <h1>I Love JSX!</h1>;
+ReactDOM.render(myelement, document.getElementById('root'));

5f318e3028348ce5f177d12547a9758b806efeb3
Manik Hosen <rahathossenmanik@gmail.com>
2019-07-20 17:39:41 +0600

    React Styles

---
+++ reactcamelcasestyle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class MyHeader extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1 style={{backgroundColor: "lightblue"}}>Hello Style!</h1>
+      <p>Add a little style!.</p>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<MyHeader />, document.getElementById('root'));
